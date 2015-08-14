5fdaea079f44b25147fa19b06ab0dc0da73e6a07
Ryan Huber <rhuber@gmail.com>
2015-08-14 12:27:18 -0400

    version bump - bugfixes

--- wee_slack.py
+++ wee_slack.py
@@ -21,7 +21,7 @@ except:
 
 SCRIPT_NAME = "slack_extension"
 SCRIPT_AUTHOR = "Ryan Huber <rhuber@gmail.com>"
-SCRIPT_VERSION = "0.98.6"
+SCRIPT_VERSION = "0.98.7"
 SCRIPT_LICENSE = "MIT"
 SCRIPT_DESC = "Extends weechat for typing notification/search/etc on slack.com"
 
