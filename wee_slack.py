8f02be20ea7c8676fe4b8433b254fa2f09f03dcc
Keith Smiley <keithbsmiley@gmail.com>
2015-12-22 19:44:55 -0800

    Cleanup message_json branch
    This removes some duplicate fallback code

--- wee_slack.py
+++ wee_slack.py
@@ -1525,11 +1525,8 @@ def render_message(message_json, force=False):
 
         if "fallback" in message_json:
             text = message_json["fallback"]
-        elif "text" in message_json:
-            if message_json['text'] is not None:
-                text = message_json["text"]
-            else:
-                text = u""
+        elif message_json['text'] is not None:
+            text = message_json["text"]
         else:
             text = u""
 
