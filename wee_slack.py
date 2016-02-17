fb9bfd685a7d1715300fb21fcc8c0eb4d157aac5
Richard Connon <richard@connon.me.uk>
2016-02-17 11:43:20 +0000

    Add config parameter to merge server buffer with weechat core buffer

--- wee_slack.py
+++ wee_slack.py
@@ -256,6 +256,8 @@ class SlackServer(object):
     def create_local_buffer(self):
         if not w.buffer_search("", self.server_buffer_name):
             self.buffer = w.buffer_new(self.server_buffer_name, "buffer_input_cb", "", "", "")
+            if w.config_string_to_boolean(w.config_get_plugin('server_buffer_merge')):
+                w.buffer_merge(self.buffer, w.buffer_search_main())
             w.buffer_set(self.buffer, "nicklist", "1")
 
     def create_slack_websocket(self, data):
@@ -2112,6 +2114,8 @@ if __name__ == "__main__":
                 w.config_set_plugin('colorize_nicks', "1")
             if not w.config_get_plugin('colorize_private_chats'):
                 w.config_set_plugin('colorize_private_chats', "0")
+            if not w.config_get_plugin('server_buffer_merge'):
+                w.config_set_plugin('server_buffer_merge', "0")
             if not w.config_get_plugin('trigger_value'):
                 w.config_set_plugin('trigger_value', "0")
             if not w.config_get_plugin('unfurl_ignore_alt_text'):
