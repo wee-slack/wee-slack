aef1083ed314295d50d19ca1db37dc2994fdbc5d
Ryan Huber <rhuber@gmail.com>
2017-02-01 12:17:59 -0800

    run every json file through eventrouter and see which events we don't handle

---
+++ _pytest/test_everything.py
@@ -0,0 +1,47 @@
+import glob
+import json
+
+#from wee_slack import render
+from wee_slack import ProcessNotImplemented
+
+def test_process_message(monkeypatch, realish_eventrouter, mock_websocket):
+
+    eventrouter = realish_eventrouter
+
+    t = eventrouter.teams.keys()[0]
+    #u = eventrouter.teams[t].users.keys()[0]
+
+    #user = eventrouter.teams[t].users[u]
+    #print user
+
+    socket = mock_websocket
+    eventrouter.teams[t].ws = socket
+
+    datafiles = glob.glob("_pytest/data/websocket/*.json")
+
+    print datafiles
+    #assert False
+
+    notimplemented = []
+
+    for fname in datafiles:
+        try:
+            data = json.loads(open(fname, 'r').read())
+            socket.add(data)
+            eventrouter.receive_ws_callback(t)
+            eventrouter.handle_next()
+        except ProcessNotImplemented as e:
+            notimplemented += [str(e)]
+        #this handles some message data not existing - need to fix
+        except KeyError:
+            pass
+
+    if len(notimplemented) > 0:
+        print "####################"
+        print notimplemented
+        print "####################"
+    assert False
+
+
+
+
