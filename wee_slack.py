221dfc528d7df5460b5d0014d28a6dd6f733ca7b
Trygve Aaberge <trygveaa@gmail.com>
2018-03-03 16:24:13 +0100

    Release v2.0.0

--- wee_slack.py
+++ wee_slack.py
@@ -31,7 +31,7 @@ except:
 
 SCRIPT_NAME = "slack"
 SCRIPT_AUTHOR = "Ryan Huber <rhuber@gmail.com>"
-SCRIPT_VERSION = "1.99"
+SCRIPT_VERSION = "2.0.0"
 SCRIPT_LICENSE = "MIT"
 SCRIPT_DESC = "Extends weechat for typing notification/search/etc on slack.com"
 
