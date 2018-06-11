eea510fb7839a0269fc637a9d91bb935bef60486
Trygve Aaberge <trygveaa@gmail.com>
2018-06-11 21:19:03 +0200

    Don't raise exception on missing event callbacks (#589)
    These missing callbacks are not really errors or exceptions, and we
    don't need to implement all of them. Raising an exception makes the
    users think something isn't working, and is confusing.

--- _pytest/test_everything.py
+++ _pytest/test_everything.py
@@ -1,50 +1,21 @@
 import glob
 import json
 
-#from wee_slack import render
-from wee_slack import ProcessNotImplemented
-
 def test_everything(realish_eventrouter, mock_websocket):
 
     eventrouter = realish_eventrouter
 
     t = eventrouter.teams.keys()[0]
-    #u = eventrouter.teams[t].users.keys()[0]
-
-    #user = eventrouter.teams[t].users[u]
-    #print user
 
     socket = mock_websocket
     eventrouter.teams[t].ws = socket
 
     datafiles = glob.glob("_pytest/data/websocket/*.json")
 
-    print datafiles
-    #assert False
-
-    notimplemented = set()
-
     for fname in datafiles:
-        try:
-            print "####################"
-            data = json.loads(open(fname, 'r').read())
-            socket.add(data)
-            print data
-            eventrouter.receive_ws_callback(t)
-            eventrouter.handle_next()
-        except ProcessNotImplemented as e:
-            notimplemented.add(str(e))
-        #this handles some message data not existing - need to fix
-        except KeyError:
-            pass
+        data = json.loads(open(fname, 'r').read())
+        socket.add(data)
+        eventrouter.receive_ws_callback(t)
+        eventrouter.handle_next()
 
-    if len(notimplemented) > 0:
-        print "####################"
-        print sorted(notimplemented)
-        print "####################"
-
-    print len(eventrouter.queue)
-    #assert False
-
-
-
+    assert len(eventrouter.queue) == 14
