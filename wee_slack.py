7e9ef8814fc1e0c374e3f55523c03613902d8327
pasja <pasja@users.noreply.github.com>
2018-11-03 21:24:29 +0100

    Match nicks with parenthesis and/or unicode (#649)
    * Fix for #570, also incorporates #545
    
    * Fixes from the review
    
    * review fix #2

--- wee_slack.py
+++ wee_slack.py
@@ -2913,7 +2913,7 @@ def linkify_text(message, team, channel):
         .replace('>', '&gt;')
         .split(' '))
     for item in enumerate(message):
-        targets = re.match('^\s*([@#])([\w.-]+[\w. -])(\W*)', item[1])
+        targets = re.match('^\s*([@#])([\w\(\).-]+)(\W*)', item[1], re.UNICODE)
         if targets and targets.groups()[0] == '@':
             named = targets.groups()
             if named[1] in ["group", "channel", "here"]:
