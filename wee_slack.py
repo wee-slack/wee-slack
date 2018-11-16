5146b1f9166bb99344b923eef859d9bd7127737d
Trygve Aaberge <trygveaa@gmail.com>
2018-11-16 15:49:56 +0100

    Add instructions on how to request slack app installation
    This is for users with a team that has restricted which apps that can be
    installed in Slack.

--- wee_slack.py
+++ wee_slack.py
@@ -3346,13 +3346,13 @@ def command_register(data, current_buffer, args):
     if args == 'register':
         message = textwrap.dedent("""
             #### Retrieving a Slack token via OAUTH ####
-
             1) Paste this into a browser: https://slack.com/oauth/authorize?client_id=2468770254.51917335286&scope=client
             2) Select the team you wish to access from wee-slack in your browser.
             3) Click "Authorize" in the browser **IMPORTANT: the redirect will fail, this is expected**
+               If you get a message saying you are not authorized to install wee-slack, the team has restricted Slack app installation and you will have to request it from an admin. To do that, go to https://my.slack.com/apps/A1HSZ9V8E-wee-slack and click "Request to Install".
             4) Copy the "code" portion of the URL to your clipboard
             5) Return to weechat and run `/slack register [code]`
-        """)
+        """).strip()
         w.prnt("", message)
         return
 
