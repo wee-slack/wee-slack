13d299a1276efac721d0009b4be1e5e557477430
David Golden <xdg@xdg.me>
2019-01-25 17:28:10 -0500

    Add show_buflist_presence config option (#558)
    This commit adds a `show_presence_presence` config option that defaults to
    true.  When true, the `+` appears before present users in the buffer
    list, as they did before this commit.  But when false, the `+` mark is
    omitted and presence is no longer shown.
    
    This helps users who want their terminal programs to flag activity but
    don't consider presence changes to be activity worth flagging.

--- wee_slack.py
+++ wee_slack.py
@@ -1838,10 +1838,9 @@ class SlackDMChannel(SlackChannel):
             print_color = self.color
         else:
             print_color = ""
-        if not present:
-            prepend = " "
-        else:
-            prepend = "+"
+        prepend = ""
+        if config.show_buflist_presence:
+            prepend = "+" if present else " "
         select = {
             "default": self.slack_name,
             "sidebar": prepend + self.slack_name,
@@ -4054,6 +4053,9 @@ class PluginConfig(object):
             default='false',
             desc='Use `foo.#channel` rather than `foo.slack.com.#channel` as the'
             ' internal name for Slack buffers. Overrides server_aliases.'),
+        'show_buflist_presence': Setting(
+            default='true',
+            desc='Display a `+` character in the buffer list for present users.'),
         'show_reaction_nicks': Setting(
             default='false',
             desc='Display the name of the reacting user(s) alongside each reactji.'),
