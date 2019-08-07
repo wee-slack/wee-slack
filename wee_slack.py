42850c14a7108547323ca393a1f7ab2195efa8ed
Trygve Aaberge <trygveaa@gmail.com>
2019-08-07 19:35:12 +0200

    Don't add deleted users to tab completion
    Fixes #703

--- wee_slack.py
+++ wee_slack.py
@@ -993,7 +993,7 @@ def nick_completion_cb(data, completion_item, current_buffer, completion):
 
     for member in current_channel.members:
         user = current_channel.team.users.get(member)
-        if user:
+        if user and not user.deleted:
             w.hook_completion_list_add(completion, "@" + user.name, 1, w.WEECHAT_LIST_POS_SORT)
     return w.WEECHAT_RC_OK
 
