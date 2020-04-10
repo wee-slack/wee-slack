wee-slack/wee-slack

commit 324890c3d0155a700fb247f5ee8f3b0778d83fe5

Simplify passing ws metadata

wee_slack.py
@@ -503,10 +503,7 @@ class EventRouter(object):
                 return w.WEECHAT_RC_OK
 
             message_json = json.loads(data.decode('utf-8'))
-            metadata = WeeSlackMetadata({
-                "team": team_hash,
-            }).jsonify()
-            message_json["wee_slack_metadata"] = metadata
+            message_json["wee_slack_metadata_team"] = team
             if self.recording:
                 self.record_event(message_json, 'type', 'websocket')
             self.receive(message_json)
@@ -638,20 +635,16 @@ class EventRouter(object):
                     function_name = "unknown"
 
                 # Here we are passing the actual objects. No more lookups.
-                meta = j.get("wee_slack_metadata")
-                if meta:
+                team = j.get("wee_slack_metadata_team")
+                if team:
                     try:
-                        if isinstance(meta, basestring):
-                            dbg("string of metadata")
-                        team = meta.get("team")
-                        if team:
-                            kwargs["team"] = self.teams[team]
-                            if "user" in j:
-                                kwargs["user"] = self.teams[team].users[j["user"]]
-                            if "channel" in j:
-                                kwargs["channel"] = self.teams[team].channels[j["channel"]]
-                            if "subteam" in j:
-                                kwargs["subteam"] = self.teams[team].subteams[j["subteam"]]
+                        kwargs["team"] = team
+                        if "user" in j:
+                            kwargs["user"] = team.users[j["user"]]
+                        if "channel" in j:
+                            kwargs["channel"] = team.channels[j["channel"]]
+                        if "subteam" in j:
+                            kwargs["subteam"] = team.subteams[j["subteam"]]
                     except:
                         dbg("metadata failure")
 
@@ -2522,18 +2515,6 @@ class SlackThreadMessage(SlackMessage):
     def __init__(self, parent_message, *args):
         super(SlackThreadMessage, self).__init__(*args)
         self.parent_message = parent_message
-
-
-class WeeSlackMetadata(object):
-    """
-    A simple container that we pickle/unpickle to hold data.
-    """
-
-    def __init__(self, meta):
-        self.meta = meta
-
-    def jsonify(self):
-        return self.meta
 
 
 class Hdata(object):

commit c7f9b3e802be96cbce60399b0d5b367f13cc1275

Set team in message_json after recording event
SlackTeam can't be json serialized, so this broke record_events. I broke
this in commit 324890c when I changed from storing the team hash on the
message_json to storing the team.

You can't currently see which team the message is from, but you couldn't
really with the team hash either since the relation between the team and
the team hash is not printed anywhere. I will add a better way to see
which team the message is from in the next commit.

Fixes #761

wee_slack.py
@@ -516,9 +516,9 @@ class EventRouter(object):
                 return w.WEECHAT_RC_OK
 
             message_json = json.loads(data.decode('utf-8'))
-            message_json["wee_slack_metadata_team"] = team
             if self.recording:
                 self.record_event(message_json, 'type', 'websocket')
+            message_json["wee_slack_metadata_team"] = team
             self.receive(message_json)
         return w.WEECHAT_RC_OK
 