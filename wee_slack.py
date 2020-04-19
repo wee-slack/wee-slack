wee-slack/wee-slack

commit b2618afd56650b3154726c216723756b327d7f48

Don't read all options when one changes

wee_slack.py
@@ -5068,11 +5068,16 @@ class PluginConfig(object):
     def __str__(self):
         return "".join([x + "\t" + str(self.settings[x]) + "\n" for x in self.settings.keys()])
 
-    def config_changed(self, data, key, value):
-        for key in self.settings:
+    def config_changed(self, data, full_key, value):
+        if full_key is None:
+            for key in self.settings:
+                self.settings[key] = self.fetch_setting(key)
+        else:
+            key = full_key.replace(CONFIG_PREFIX + ".", "")
             self.settings[key] = self.fetch_setting(key)
-        if self.debug_mode:
-            create_slack_debug_buffer()
+            if full_key == CONFIG_PREFIX + ".debug_mode":
+                if self.debug_mode:
+                    create_slack_debug_buffer()
         return w.WEECHAT_RC_OK
 
     def fetch_setting(self, key):

commit 47af4ac2687967ceda1d4112536819275c4d5b1c

Fix debug buffer not being created on script load
This bug was introduced in commit b2618af.

wee_slack.py
@@ -5071,9 +5071,9 @@ class PluginConfig(object):
         else:
             key = full_key.replace(CONFIG_PREFIX + ".", "")
             self.settings[key] = self.fetch_setting(key)
-            if full_key == CONFIG_PREFIX + ".debug_mode":
-                if self.debug_mode:
-                    create_slack_debug_buffer()
+
+        if (full_key is None or full_key == CONFIG_PREFIX + ".debug_mode") and self.debug_mode:
+            create_slack_debug_buffer()
         return w.WEECHAT_RC_OK
 
     def fetch_setting(self, key):