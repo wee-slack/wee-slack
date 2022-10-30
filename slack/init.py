675ad9bb16e4041afde43f551d7a76bb9b008f5e
Trygve Aaberge <trygveaa@gmail.com>
2022-10-30 07:49:52 +0100

    Move all into slack directory

--- slack/init.py
+++ slack/init.py
@@ -1,7 +1,14 @@
 import weechat
 
-from slack.config import SlackWorkspace
+from slack.config import SlackConfig, SlackWorkspace
 from slack.shared import shared
+from slack.task import create_task
+from slack.util import get_callback_name
+
+
+def shutdown_cb():
+    weechat.config_write(shared.config.weechat_config.pointer)
+    return weechat.WEECHAT_RC_OK
 
 
 async def init():
@@ -18,3 +25,20 @@ async def init():
     print(workspace)
     print(workspace.config.slack_timeout.value)
     print(shared.config.color.reaction_suffix.value)
+
+
+def main():
+    if weechat.register(
+        shared.SCRIPT_NAME,
+        shared.SCRIPT_AUTHOR,
+        shared.SCRIPT_VERSION,
+        shared.SCRIPT_LICENSE,
+        shared.SCRIPT_DESC,
+        get_callback_name(shutdown_cb),
+        "",
+    ):
+        shared.weechat_version = int(weechat.info_get("version_number", "") or 0)
+        shared.workspaces = {}
+        shared.config = SlackConfig()
+        shared.config.config_read()
+        create_task(init(), final=True)
