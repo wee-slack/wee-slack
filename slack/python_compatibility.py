7ab59b24a43b1219aac5caba59188934bdf69e09
Trygve Aaberge <trygveaa@gmail.com>
2023-01-28 21:53:10 +0100

    Fix compatibility with Python 3.8

---
+++ slack/python_compatibility.py
@@ -0,0 +1,14 @@
+# Copied from https://peps.python.org/pep-0616/ for support for Python < 3.9
+def removeprefix(self: str, prefix: str) -> str:
+    if self.startswith(prefix):
+        return self[len(prefix) :]
+    else:
+        return self[:]
+
+
+# Copied from https://peps.python.org/pep-0616/ for support for Python < 3.9
+def removesuffix(self: str, suffix: str) -> str:
+    if suffix and self.endswith(suffix):
+        return self[: -len(suffix)]
+    else:
+        return self[:]
