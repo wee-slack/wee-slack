13a42d114858f31c1df27cf964cb25d8cc2bd722
Trygve Aaberge <trygveaa@gmail.com>
2023-01-31 20:04:20 +0100

    Don't set future result in __await__

--- tests/test_sleep.py
+++ tests/test_sleep.py
@@ -13,11 +13,12 @@ def test_sleep(mock_method: MagicMock):
     coroutine = sleep(milliseconds)
     future = coroutine.send(None)
     assert isinstance(future, FutureTimer)
+    future.set_result((0,))
 
     mock_method.assert_called_once_with(
         milliseconds, 0, 1, get_callback_name(weechat_task_cb), future.id
     )
 
     with pytest.raises(StopIteration) as excinfo:
-        coroutine.send((0,))
+        coroutine.send(None)
     assert excinfo.value.value == (0,)  # TODO: Will probably change to None
