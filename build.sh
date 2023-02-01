531f376274ba214f3b414ee92ae38f1185340f55
Trygve Aaberge <trygveaa@gmail.com>
2023-02-01 22:56:27 +0100

    Handle multiline and indented imports in build.sh
    This is pretty hacky, the awk script joins some lines it shouldn't so I
    should look into that, but now it works without manual changes at least.
    
    Any indented import is assumed to be within `if TYPE_CHECKING` and
    replaced with `pass`.

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-contents="$(cat slack/*.py main.py | grep -Ev '^from slack[. ]')"
+shopt -s extglob
+
+contents="$(cat slack/util.py slack/task.py slack/!(util|task).py main.py | \
+  awk -v RS='\\([^)]+\\)' '/from.*import/ {gsub(/[[:space:]]+/, "", RT)} {ORS=RT} 1' | \
+  grep -Ev '^from slack[. ]')"
 
 echo "$contents" | grep '^from __future__' | sort -u > build/slack.py
 echo "$contents" | grep -v '^from __future__' | grep -E '^(import|from)' | sort -u >> build/slack.py
-echo "$contents" | grep -Ev '^(import|from)' >> build/slack.py
+echo "$contents" | grep -Ev '^(import|from)' | sed 's/^\( \+\)\(import\|from\).*/\1pass/' >> build/slack.py
