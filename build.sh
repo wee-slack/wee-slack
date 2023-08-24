88c411960684fb7403e09e0f9393c0aaf64797bc
Trygve Aaberge <trygveaa@gmail.com>
2023-08-24 21:08:10 +0200

    Fix build script

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 shopt -s extglob
 
 contents="$(cat slack/python_compatibility.py slack/util.py slack/task.py slack/!(python_compatibility|util|task).py main.py | \
-  perl -0777 -pe 's/^( *from [^(\n]+\([^)]+\))/$1=~s|\s+| |gr/mge' | \
+  perl -0777 -pe 's/^( *from [^(\n]+\([^)]+\))/$1=~s|\n+||gr/mge' | \
   grep -Ev '^from slack[. ]')"
 
 (
@@ -11,6 +11,6 @@ contents="$(cat slack/python_compatibility.py slack/util.py slack/task.py slack/
   echo "# For the original source, see https://github.com/wee-slack/wee-slack"
   echo
   echo "$contents" | grep '^from __future__' | sort -u
-  echo "$contents" | grep -v '^from __future__' | grep -E '^(import|from)' | sort -u
-  echo "$contents" | grep -Ev '^(import|from)' | sed 's/^\( \+\)\(import\|from\).*/\1pass/'
+  echo "$contents" | grep -v '^from __future__' | grep -E '^(import|from) ' | sort -u
+  echo "$contents" | grep -Ev '^(import|from) ' | sed 's/^\( \+\)\(import\|from\) .*/\1pass/'
 ) > build/slack.py
