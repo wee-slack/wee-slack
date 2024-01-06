6b5f46d2e1550d94a9beee4b74dc5ba2b2ad5c2e
Trygve Aaberge <trygveaa@gmail.com>
2024-01-06 17:02:32 +0100

    Use the api_cookie value as the `d` cookie if no keys are specified
    Usually, only the `d` cookie is needed, so this allows you to only
    specify the value of that cookie without the key if you don't need to
    set any other cookies.

--- tests/test_get_cookies.py
+++ tests/test_get_cookies.py
@@ -1,6 +1,16 @@
 from slack.util import get_cookies
 
 
+def test_get_cookies_without_key_value_encoded():
+    cookie = get_cookies("a%2Bb")
+    assert cookie == "d=a%2Bb"
+
+
+def test_get_cookies_without_key_value_not_encoded():
+    cookie = get_cookies("a+b")
+    assert cookie == "d=a%2Bb"
+
+
 def test_get_cookies_multiple_keys_value_encoded():
     cookie = get_cookies("d=a%2Bb ; d-s=1")
     assert cookie == "d=a%2Bb; d-s=1"
