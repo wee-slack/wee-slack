7679e0742239223a2ea142ecbf57f8a10032ec3b
Trygve Aaberge <trygveaa@gmail.com>
2024-02-18 12:42:29 +0100

    Rename clean option in status to clear

--- slack/commands.py
+++ slack/commands.py
@@ -366,12 +366,12 @@ def command_slack_debug(buffer: str, args: List[str], options: Options):
         print_uncaught_error(error, True, options)
 
 
-@weechat_command("-clean")
+@weechat_command("-clear")
 def command_slack_status(buffer: str, args: List[str], options: Options):
     status = args[0]
     slack_buffer = shared.buffers.get(buffer)
     if slack_buffer is not None:
-        if options.get("clean"):
+        if options.get("clear"):
             run_async(slack_buffer.workspace.api.clear_user_status())
         elif slack_buffer and len(status) > 0:
             run_async(slack_buffer.workspace.api.set_user_status(status))
